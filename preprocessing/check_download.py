ccd29442866e783f9b9d9517d68afbf349aa8f69
Felipe Moreira <felipemoreira.physics@gmail.com>
2026-07-13 15:48:40 -0300

    add

--- preprocessing/check_download.py
+++ preprocessing/check_download.py
@@ -5,16 +5,28 @@ from pathlib import Path
 path_data = Path('../data/')
 
 data_parameters = pd.read_csv('gw_filtered_events.csv')
-folder = [p.name for p in path_data.iterdir() if p.is_dir()]
+folders = [p.name for p in path_data.iterdir() if p.is_dir()]
 
-Nfolder = len(folder)
+Nfolder = len(folders)
 Nparame = len(data_parameters)
 
-print(f'N folder = {len(folder)}')
-print(f'N parame = {Nparame}')
 
-
+empty = 0
 for i in range(Nfolder):
-    path = f'{path_data}/{folder[i]}'
+    # 1. Cria a string do caminho
+    path_str = f'{path_data}/{folders[i]}'
+    
+    # 2. Transforma essa string em um objeto Path do pathlib
+    path_objeto = Path(path_str)
 
-    esta_vazio = not any(path.iterdir())
+    tem_hdf5 = next(path_objeto.glob('*.hdf5'), None) is not None
+    is_empty = not tem_hdf5
+
+    if is_empty:
+        empty+=1
+
+
+print(f'N parame = {Nparame}\n')
+print(f'N folder = {len(folders)}')
+print(f'Empty folder = {empty}')
+print(f'Full folder = {Nfolder-empty}')
